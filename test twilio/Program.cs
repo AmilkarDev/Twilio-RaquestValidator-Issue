f5171547bfe6830f3461e48b7a71d876b0f26979
Malek <melek.ferhi@gmail.com>
2022-04-15 06:31:21 -0400

    Add project files.

---
+++ test twilio/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using Twilio;
+using Twilio.Rest.Api.V2010.Account;
+
+namespace test_twilio
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Find your Account SID and Auth Token at twilio.com/console
+            // and set the environment variables. See http://twil.io/secure
+            string accountSid = "*****";
+            string authToken = "******";
+
+            TwilioClient.Init(accountSid, authToken);
+            var requestValidator = new Twilio.Security.RequestValidator(authToken);
+            var ss  = requestValidator.GetHashCode();
+            var message = MessageResource.Create(
+                body: "NON",
+                from: new Twilio.Types.PhoneNumber("+1**********"),
+                to: new Twilio.Types.PhoneNumber("+1**********")
+            );
+
+            Console.WriteLine(message.Sid);
+        }
+    }
+}
---
+++ testTwilio2/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using Twilio;
+using Twilio.Rest.Api.V2010.Account;
+using Twilio.Security;
+namespace testTwilio2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string accountSid = "*******";
+            string authToken = "******";
+
+            TwilioClient.Init(accountSid, authToken);
+            var requestValidator = new RequestValidator(authToken);
+             var ss = requestValidator.GetHashCode();
+            var message = MessageResource.Create(
+                body: "NON",
+                from: new Twilio.Types.PhoneNumber("+1*******"),
+                to: new Twilio.Types.PhoneNumber("+1******")
+            );
+
+            Console.WriteLine(message.Sid);
+        }
+    }
+}
